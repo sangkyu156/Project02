ceccf2be4d7c23c07590c45b087369380f538aa1
sangkyu <sangkyu156@naver.com>
2022-12-28 00:43:57 +0900

    가중치랜덤으로 스킬생성 가능, 다시뽑기 가능

--- Scripts/Manager/ItemManager.cs
+++ Scripts/Manager/ItemManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ItemManager : MonoBehaviour
 {
     GameObject[] items;
+    string[] skillNameArray; //스킬명 모와두는곳
+    int[] skillWeightedArray; //스킬 가중치 모와두는곳
+    WRandom.WeightedRandomPicker<string> weightedRandom = new WRandom.WeightedRandomPicker<string>(); //'가중치랜덤' 변수 생성 & 초기화
 
     private void Awake()
     {
@@ -17,24 +21,79 @@ public class ItemManager : MonoBehaviour
 
     void Start()
     {
-        GameObject prefeb;
-        GameObject skill;
-        for (int i = 0; i < items.Length; i++)
-        {
-            prefeb = Resources.Load<GameObject>($"Skills/Fire_{i+1}");//프리펩 찾음
-            skill = Instantiate(prefeb);//프리펩 생성
-            skill.transform.SetParent(items[i].transform, false);//부모 지정
-        }
+        AddSkills();
+        SetSkills();
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            Redraw();
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Item.itemAction();
+        }
     }
 
-    void SetItem()
+    //랜덤으로 뽑을 스킬 추가
+    void AddSkills()
     {
-        WRandom.WeightedRandomPicker<string> randomPicker;
-        //제네릭 클래스 좀더 공부하고 가중치랜덤으로 확률 바꿔가면서 시작할때 상점에 스킬 배치해보기
+        skillNameArray = new string[Enum.GetValues(typeof(SkillData.SKILLS)).Length];
+        skillWeightedArray = new int[Enum.GetValues(typeof(SkillData.SKILLS)).Length];
+
+        Debug.Log($"스킬 개수 = {Enum.GetValues(typeof(SkillData.SKILLS)).Length}");
+
+        skillNameArray = typeof(SkillData.SKILLS).GetEnumNames();//'SKILLS'에서 이름 있는 것들만 뽑아오기 | skillNameArray 세팅
+        SetSkillWeightedArray();//스킬 가중치 모와두기 | skillWeightedArray 세팅
+
+        //스킬,가중치 하나씩 추가 (가중치가 높을수록 잘뽑힘)
+        for (int i = 0; i < skillNameArray.Length; i++)
+        {
+            Debug.Log($"[{i + 1}]번째 스킬 이름 = {skillNameArray[i]}\n[{i+1}]번째 스킬 가중치 = {skillWeightedArray[i]}");
+            weightedRandom.Add($"{skillNameArray[i]}", skillWeightedArray[i]);
+        }
+    }
+
+    //상점 팝업에 랜덤으로 스킬 배치
+    void SetSkills()
+    {
+        GameObject prefeb;
+        GameObject skill;
+        string skillName = "";
+        List<string> skillList = new List<string>();
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            skillName = weightedRandom.GetRandomPick();
+            skillList.Add(skillName);
+
+            prefeb = Resources.Load<GameObject>($"Skills/{skillName}");//프리펩 찾음
+            skill = Instantiate(prefeb);//프리펩 생성
+            skill.transform.SetParent(items[i].transform, false);//부모 지정
+            skill.transform.SetAsFirstSibling();//버튼은 RectTransform 이라 내려도 GameObject가 더 내려감 그래서 그냥 게임오브젝트를 젤 위로 올림
+        }
+    }
+
+    //'SkillData.SKILLS'에서 key를 하나씩 넣어서 key에 해당하는 value를 저장하는 함수
+    void SetSkillWeightedArray()
+    {
+        for (int i = 0; i < skillNameArray.Length; i++)
+        {
+            skillWeightedArray[i] = (int)Enum.Parse(typeof(SkillData.SKILLS), skillNameArray[i]);
+        }
+    }
+
+    //다시 뽑기
+    void Redraw()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            Destroy(items[i].transform.GetChild(0).gameObject);
+        }
+        SetSkills();
+        Item.itemAction();
     }
 }
--- Scripts/Util/Item.cs
+++ Scripts/Util/Item.cs
@@ -5,7 +5,7 @@ using System;
 
 public class Item : MonoBehaviour
 {
-    public GameObject buyButton;
+    public RectTransform buyButton;
     public static Action itemAction;
 
     private void Awake()
@@ -17,8 +17,9 @@ public class Item : MonoBehaviour
         SetButtonLayer();
     }
 
+    //버튼 오브젝트 제일 아래로 내려서 항상 클릭 가능하도록 하는 함수 (하지만 Skill은 GameObject라서 버튼을 내려도 새로생긴 GameObject 가 더내려가있음 그래서 그냥 GameObject를 올려줬음)
     void SetButtonLayer()
     {
-        buyButton.transform.SetAsLastSibling();
+        buyButton.SetAsLastSibling();
     }
 }
